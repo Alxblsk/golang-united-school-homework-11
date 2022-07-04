84d2b2392eca844ab7043d87236b750325a76d96
Aliaksei Belski <aliaksei.belski@epicgames.com>
2022-07-04 14:12:51 -0700

    Get Batch IDs (semaphore), first version

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,31 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	semaphore := make(chan struct{}, pool)
+
+	var i int64 = 0
+	for i = 0; i < n; i++ {
+		wg.Add(1)
+
+		semaphore <- struct{}{}
+
+		go func(id int64) {
+			defer wg.Done()
+
+			value := getOne(id)
+			<-semaphore
+
+			mu.Lock()
+			res = append(res, value)
+			mu.Unlock()
+		}(i)
+
+	}
+
+	wg.Wait()
+
+	return res
 }
